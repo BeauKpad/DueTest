a4a2df499cb7c3c0dc296dffac19f3a9e4c632e6
LeBeau Kpadenou <BeauKpad@GMail.com>
2014-09-28 04:56:23 -0400

    in the middle of fragments

---
+++ src/com/beaukpad/cashdue/StatsFrag.java
@@ -0,0 +1,29 @@
+package com.beaukpad.cashdue;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StatsFrag extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+}
